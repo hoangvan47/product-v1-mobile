bbc669ed76e753a8d39f32dc89edc4170fad80fe
hoangvan47 <vanhoang15091999@gmail.com>
2026-02-14 15:22:03 +0700

    feat: embed web mini app with flutter-webview bridge

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,136 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+const miniAppUrl = String.fromEnvironment(
+  'MINI_APP_URL',
+  defaultValue: 'https://mobile-connection-817931.framer.app/',
+);
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MiniAppHost());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MiniAppHost extends StatelessWidget {
+  const MiniAppHost({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Product V1 Mini App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF0A0A0A)),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MiniAppWebViewPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MiniAppWebViewPage extends StatefulWidget {
+  const MiniAppWebViewPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MiniAppWebViewPage> createState() => _MiniAppWebViewPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MiniAppWebViewPageState extends State<MiniAppWebViewPage> {
+  late final WebViewController _controller;
+  String _lastWebMessage = 'No message yet';
+  bool _isLoading = true;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageStarted: (_) => setState(() => _isLoading = true),
+          onPageFinished: (_) {
+            setState(() => _isLoading = false);
+            _sendToWeb(
+              type: 'flutter_ready',
+              payload: {
+                'platform': defaultTargetPlatform.name,
+                'miniApp': 'product_v1_mobile',
+              },
+            );
+          },
+          onWebResourceError: (_) => setState(() => _isLoading = false),
+        ),
+      )
+      ..addJavaScriptChannel(
+        'FlutterChannel',
+        onMessageReceived: (JavaScriptMessage message) {
+          setState(() {
+            _lastWebMessage = message.message;
+          });
+        },
+      )
+      ..loadRequest(Uri.parse(miniAppUrl));
+  }
+
+  Future<void> _sendToWeb({
+    required String type,
+    Map<String, dynamic>? payload,
+  }) async {
+    final data = jsonEncode({
+      'source': 'flutter',
+      'type': type,
+      'payload': payload ?? {},
+      'timestamp': DateTime.now().toIso8601String(),
     });
+
+    final escapedData = jsonEncode(data);
+    await _controller.runJavaScript(
+      'window.receiveFromFlutter && window.receiveFromFlutter($escapedData);',
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Mini App Host'),
+        actions: [
+          IconButton(
+            tooltip: 'Send ping to web',
+            onPressed: () {
+              _sendToWeb(
+                type: 'ping_from_flutter',
+                payload: {'message': 'Hello from Flutter mini app'},
+              );
+            },
+            icon: const Icon(Icons.send_outlined),
+          ),
+          IconButton(
+            tooltip: 'Reload',
+            onPressed: () => _controller.reload(),
+            icon: const Icon(Icons.refresh),
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: Column(
+        children: [
+          if (_isLoading) const LinearProgressIndicator(minHeight: 2),
+          Expanded(child: WebViewWidget(controller: _controller)),
+          Container(
+            width: double.infinity,
+            color: const Color(0xFFF2F2F2),
+            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+            child: Text(
+              'Web -> Flutter: $_lastWebMessage',
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(fontSize: 12, color: Colors.black87),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
